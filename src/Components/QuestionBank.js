b4dd4564d8affea1d1cd2019a04be168eba70e83
buterajacques1 <jbutera702@gmail.com>
2024-05-30 17:41:30 +0200

    quiz app

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,47 @@
+const qBank = [
+    {
+      id: 1,
+      question: "Who is the founder and CEO of JABU CLOTHING BRAND?",
+      options: ["Jaques BUTERA", "Paul KAGAME", "Fred Swaniker", "Bill gates"],
+      answer: "Jaques BUTERA",
+    },
+    {
+      id: 2,
+      question: "In which year was JABU CLOTHING BRAND founded?",
+      options: ["2010", "2022", "2000", "2024"],
+      answer: "2022",
+    },
+    {
+      id: 3,
+      question: "Who is the production manager of JABU CLOTHING BRAND?",
+      options: ["BEBE Joseline", "Joseline DUSIBAMA", "Emanuel DUSIBAMA", "Domitille UWIZEYE"],
+      answer: "BEBE Joseline",
+    },
+    {
+      id: 4,
+      question: "Who is the best friend of JABU CLOTHIND BRAND's CEO?",
+      options: ["Fred Swaniker", "Paul KAGAME", "ENZO....", "His mother", "MUGAGA", "Marcelin SIBOMANA"],
+      answer: "His mother",
+    },
+    {
+      id: 5,
+      question: "Who is the best customer of JABU CLOTHING BRAND?",
+      options: ["ALU", "Kigali Fit gym", "REMERA catholic school", "Marcelin"],
+      answer: "ALU",
+    },
+    {
+      id: 6,
+      question: "Who is the marketing manager of JABU CLOTHING BRAND?",
+      options: ["Ayamar Audry IRUTINGABO", "Frank MUPENZI", "Fred MUGABO", "Miss MUHETO"],
+      answer: "Ayamar Audry IRUTINGABO",
+    },
+    {
+      id: 7,
+      question: "What is the JABU clothing brand's goal by 2027?",
+      options: ["conquering monopoly power", "Eat small and sleep early", "Shy awy from taxes", "By 2027, Jabu aims to become the leading clothing brand in Rwanda and worldwide, while ensuring that 85% of its total workforce is comprised of women."],
+      answer: "By 2027, Jabu aims to become the leading clothing brand in Rwanda and worldwide, while ensuring that 85% of its total workforce is comprised of women.",
+    },
+  ];
+  
+  export default qBank;
+  
